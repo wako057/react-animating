e5bcc2560971f63f8b748cb7e26e3aad8224e785
Gregory Gerard <wako057@gmail.com>
2024-09-23 11:44:01 +0200

    feat: animate disappearance / exit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,6 +10,7 @@ export default function Modal({ title, children, onClose }) {
         className="modal"
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }}
       >
         <h2>{title}</h2>
         {children}
